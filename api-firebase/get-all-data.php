82a7c7b6842c53ef219d05dc6ecc866bcfcc50d7
Akraja712 <ashokkumar7hitter@gmail.com>
2024-07-22 17:52:08 +0530

    get-all-data.php

--- api-firebase/get-all-data.php
+++ api-firebase/get-all-data.php
@@ -32,15 +32,17 @@ if (isset($config['system_timezone']) && isset($config['system_timezone_gmt'])) 
     $db->sql("SET `time_zone` = '+05:30'");
 }
 
+if (!verify_token()) {
+    return false;
+}
 
-
-/*if (!isset($_POST['accesskey'])  || trim($_POST['accesskey']) != $access_key) {
+if (!isset($_POST['accesskey'])  || trim($_POST['accesskey']) != $access_key) {
     $response['error'] = true;
     $response['message'] = "No Accsess key found!";
     print_r(json_encode($response));
     return false;
     exit();
-}*/
+}
 
 /* 
 get-all-data.php
